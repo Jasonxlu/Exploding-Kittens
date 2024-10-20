df5dd094a2c48b65264cf3f4bc8ab8e689d6fa06
Isa Gonzalez <isaegonzalez03@gmail.com>
2024-10-19 20:21:48 -0500

    setUpPlayers_OnePlayerOneName_ThrowException passes

---
+++ src/main/java/explodingwildcats/ExplodingWildcats.java
@@ -0,0 +1,8 @@
+package explodingwildcats;
+
+public class ExplodingWildcats {
+
+    public void setUpPlayers(int numberOfPlayers, String[] names) {
+        throw new IllegalArgumentException("Not enough players");
+    }
+}
---
+++ src/test/java/explodingwildcats/ExplodingWildcatsTests.java
@@ -0,0 +1,22 @@
+package explodingwildcats;
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ExplodingWildcatsTests {
+    @Test
+    public void setUpPlayers_OnePlayerOneName_ThrowException() {
+        ExplodingWildcats game = new ExplodingWildcats();
+
+        int numPlayers = 1;
+        String[] names = {"John"};
+
+        String expectedMessage = "Not enough players";
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            game.setUpPlayers(numPlayers, names);
+        });
+
+        String actualMessage = exception.getMessage();
+        assertEquals(expectedMessage, actualMessage);
+    }
+}
