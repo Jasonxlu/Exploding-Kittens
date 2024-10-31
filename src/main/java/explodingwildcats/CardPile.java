ce512858433c1995eba5b134d664ae9a9dbc551d
Isa Gonzalez <isaegonzalez03@gmail.com>
2024-10-31 10:09:09 -0500

    Removed CardPile function implementations

--- src/main/java/explodingwildcats/CardPile.java
+++ src/main/java/explodingwildcats/CardPile.java
@@ -3,17 +3,9 @@ package explodingwildcats;
 import java.util.ArrayList;
 
 public class CardPile {
-    private final ArrayList<Card> cards = new ArrayList<Card>();
+    public void AddCard(Card c) {}
 
-    public void AddCard(Card c) {
-        cards.add(c);
-    }
+    public Card[] getCards() { return new Card[0]; }
 
-    public Card[] getCards() {
-        return cards.toArray(new Card[0]);
-    }
-
-    public Card popCard() {
-        return cards.remove(0);
-    }
+    public Card popCard() { return null; }
 }
