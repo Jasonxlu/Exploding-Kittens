5836306fb6b307a2954b6aa229744a3d4cb32536
Isa Gonzalez <isaegonzalez03@gmail.com>
2024-10-31 10:09:28 -0500

    Removed Player class implementations

--- src/main/java/explodingwildcats/Player.java
+++ src/main/java/explodingwildcats/Player.java
@@ -3,22 +3,11 @@ package explodingwildcats;
 import java.util.ArrayList;
 
 public class Player {
-    private String name;
-    private final CardPile hand = new CardPile();
+    public Player(String name) {}
 
-    public Player(String name) {
-        this.name = name;
-    }
+    public String getName() { return ""; }
 
-    public String getName() {
-        return name;
-    }
+    public void AddCardToHand(Card c) {}
 
-    public void AddCardToHand(Card c) {
-        this.hand.AddCard(c);
-    }
-
-    public Card[] getHand() {
-        return this.hand.getCards();
-    }
+    public Card[] getHand() { return new Card[0]; }
 }
