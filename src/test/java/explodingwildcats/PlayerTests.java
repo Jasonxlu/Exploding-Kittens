d20682e2bcd0e38345a42d753bcb6983e3ba786f
Brennan Benson <brennankbenson@gmail.com>
2024-11-04 20:36:38 -0600

    addCardToHand_SEE_THE_FUTURE passes

--- src/test/java/explodingwildcats/PlayerTests.java
+++ src/test/java/explodingwildcats/PlayerTests.java
@@ -62,4 +62,18 @@ public class PlayerTests {
 
     EasyMock.verify(hand);
   }
+
+  @Test
+  public void addCardToHand_SEE_THE_FUTURE() {
+    CardPile hand = EasyMock.createMock(CardPile.class);
+    Player player = new Player("Bob", hand);
+    Card attackCard = Card.SEE_THE_FUTURE;
+
+    hand.addCard(attackCard);
+    EasyMock.replay(hand);
+
+    player.addCardToHand(attackCard);
+
+    EasyMock.verify(hand);
+  }
 }
