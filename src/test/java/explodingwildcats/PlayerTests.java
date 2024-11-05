d01b5264259becaca323cebda8664175482de1ef
Brennan Benson <brennankbenson@gmail.com>
2024-11-04 20:34:13 -0600

    addCardToHand_CAT passes

--- src/test/java/explodingwildcats/PlayerTests.java
+++ src/test/java/explodingwildcats/PlayerTests.java
@@ -20,4 +20,18 @@ public class PlayerTests {
 
     EasyMock.verify(hand);
   }
+
+  @Test
+  public void addCardToHand_CAT() {
+    CardPile hand = EasyMock.createMock(CardPile.class);
+    Player player = new Player("Bob", hand);
+    Card attackCard = Card.CAT;
+
+    hand.addCard(attackCard);
+    EasyMock.replay(hand);
+
+    player.addCardToHand(attackCard);
+
+    EasyMock.verify(hand);
+  }
 }
