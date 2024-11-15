8d580c82ba01aee22aed6dbb6b948f6c42b463ca
Brennan Benson <brennankbenson@gmail.com>
2024-11-14 22:17:56 -0600

    doAlterTheFuture_twoCardsInDrawPile_reorderOppositeOrder passes

--- src/test/java/explodingwildcats/TurnManagerTests.java
+++ src/test/java/explodingwildcats/TurnManagerTests.java
@@ -7,7 +7,7 @@ import ui.UserInterface;
 public class TurnManagerTests {
 
   @Test
-  public void doAlterTheFuture_oneElementInDrawPile_reorderInSamePlace() {
+  public void doAlterTheFuture_oneCardInDrawPile_reorderInSamePlace() {
     GameEngine gameEngine = EasyMock.createMock(GameEngine.class);
     UserInterface ui = EasyMock.createMock(UserInterface.class);
     TurnManager turnManager = new TurnManager(ui, gameEngine);
@@ -26,6 +26,37 @@ public class TurnManagerTests {
     EasyMock.expect(ui.promptNewOrder(1)).andReturn(newOrder);
 
     Card[] reorderedDrawPile = new Card[] { cardInDrawPile };
+    // 4. GameEngine.replaceTopDrawPileCards called with the new order converted to the cards.
+    gameEngine.replaceTopDrawPileCards(reorderedDrawPile);
+
+    EasyMock.replay(ui, gameEngine);
+
+    turnManager.doAlterTheFuture();
+
+    EasyMock.verify(ui, gameEngine);
+  }
+
+  @Test
+  public void doAlterTheFuture_twoCardsInDrawPile_reorderOppositeOrder() {
+    GameEngine gameEngine = EasyMock.createMock(GameEngine.class);
+    UserInterface ui = EasyMock.createMock(UserInterface.class);
+    TurnManager turnManager = new TurnManager(ui, gameEngine);
+
+    Card topCardInDrawPile = Card.NOPE;
+    Card secondCardInDrawPile = Card.ATTACK;
+    Card[] peekedDrawPile = new Card[] { topCardInDrawPile, secondCardInDrawPile };
+
+    // 1. GameEngine.peek() called
+    EasyMock.expect(gameEngine.peekDrawPile()).andReturn(peekedDrawPile);
+
+    // 2. ui.println called with peeked cards
+    ui.println("Top: NOPE, 2nd: ATTACK");
+    int[] newOrder = new int[] {2,1};
+
+    // 3. ui.promptNewOrder called with number of peeked cards
+    EasyMock.expect(ui.promptNewOrder(2)).andReturn(newOrder);
+
+    Card[] reorderedDrawPile = new Card[] { secondCardInDrawPile, topCardInDrawPile };
     // 4. GameEngine.replaceTopDrawPileCards called with the new order converted to the cards.
     gameEngine.replaceTopDrawPileCards(reorderedDrawPile);
 
