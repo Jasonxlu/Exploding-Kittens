19eb2efa20aad1e7f8bfaacae5bff22ba75d545a
Isa Gonzalez <isaegonzalez03@gmail.com>
2024-12-07 17:14:32 -0600

    playerHasCard_IndexFive_returnsFalse passes

--- src/test/java/explodingwildcats/GameEngineTests.java
+++ src/test/java/explodingwildcats/GameEngineTests.java
@@ -945,4 +945,41 @@ public class GameEngineTests {
 
     EasyMock.verify(playerFactory, cardPileFactory, p1, p2, p3, p4, p5, p6);
   }
+
+  @Test
+  public void playerHasCard_IndexFive_returnsFalse() {
+    PlayerFactory playerFactory = EasyMock.createMock(PlayerFactory.class);
+    CardPileFactory cardPileFactory = EasyMock.createMock(CardPileFactory.class);
+    CardPile drawPile = EasyMock.createMock(CardPile.class);
+    GameEngine game = new GameEngine(playerFactory, cardPileFactory, drawPile);
+
+    int numPlayers = 6;
+    String[] names = {"John", "Jane", "Bob", "Job", "Charlie", "David"};
+    CardPile playerHand = EasyMock.createMock(CardPile.class);
+
+    Player p1 = EasyMock.createMock(Player.class);
+    Player p2 = EasyMock.createMock(Player.class);
+    Player p3 = EasyMock.createMock(Player.class);
+    Player p4 = EasyMock.createMock(Player.class);
+    Player p5 = EasyMock.createMock(Player.class);
+    Player p6 = EasyMock.createMock(Player.class);
+
+    EasyMock.expect(cardPileFactory.createCardPile()).andReturn(playerHand).times(numPlayers);
+    EasyMock.expect(playerFactory.createPlayer("John", playerHand)).andReturn(p1);
+    EasyMock.expect(playerFactory.createPlayer("Jane", playerHand)).andReturn(p2);
+    EasyMock.expect(playerFactory.createPlayer("Bob", playerHand)).andReturn(p3);
+    EasyMock.expect(playerFactory.createPlayer("Job", playerHand)).andReturn(p4);
+    EasyMock.expect(playerFactory.createPlayer("Charlie", playerHand)).andReturn(p5);
+    EasyMock.expect(playerFactory.createPlayer("David", playerHand)).andReturn(p6);
+
+    EasyMock.expect(p6.hasCard(Card.BEARD_CAT)).andReturn(false);
+
+    EasyMock.replay(playerFactory, cardPileFactory, p1, p2, p3, p4, p5, p6);
+
+    game.setUpPlayers(numPlayers, names);
+    boolean result = game.playerHasCard(Card.BEARD_CAT, 5);
+    assertFalse(result);
+
+    EasyMock.verify(playerFactory, cardPileFactory, p1, p2, p3, p4, p5, p6);
+  }
 }
