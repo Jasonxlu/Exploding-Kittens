2eb1ea1cfd99aae789e58c65e543883432e5d843
Brennan Benson <brennankbenson@gmail.com>
2024-12-10 10:20:35 -0600

    validateComboCards_catCard3x_validCardList_validPlayer_returnCardList passes

--- src/test/java/explodingwildcats/GameEngineTests.java
+++ src/test/java/explodingwildcats/GameEngineTests.java
@@ -1521,4 +1521,38 @@ public class GameEngineTests {
 
     EasyMock.verify(game);
   }
+
+  @Test
+  public void validateComboCards_catCard3x_validCardList_validPlayer_returnCardList() {
+    PlayerFactory playerFactory = EasyMock.createMock(PlayerFactory.class);
+    CardPileFactory cardPileFactory = EasyMock.createMock(CardPileFactory.class);
+    CardPile drawPile = EasyMock.createMock(CardPile.class);
+    GameEngine game = EasyMock.partialMockBuilder(GameEngine.class)
+            .withConstructor(playerFactory, cardPileFactory, drawPile)
+            .addMockedMethod("getCardByName")
+            .addMockedMethod("getPlayerByIndex")
+            .addMockedMethod("playerHasCards")
+            .createMock();
+
+    Player player = EasyMock.createMock(Player.class);
+    int playerIndex = 0;
+
+    String c1String = "rainbow cat";
+    Card c1 = Card.BEARD_CAT;
+    String[] cardStrings = new String[] { c1String, c1String, c1String };
+    Card[] cards = new Card[] { c1, c1, c1 };
+
+    boolean playerHasC1 = true;
+
+    EasyMock.expect(game.getPlayerByIndex(playerIndex)).andReturn(player);
+    EasyMock.expect(game.getCardByName(c1String)).andReturn(c1).times(3);
+    EasyMock.expect(game.playerHasCards(c1, playerIndex, 3)).andReturn(playerHasC1);
+
+    EasyMock.replay(game);
+
+    Card[] actualCards = game.validateComboCards(cardStrings, playerIndex);
+    assertArrayEquals(cards, actualCards);
+
+    EasyMock.verify(game);
+  }
 }
