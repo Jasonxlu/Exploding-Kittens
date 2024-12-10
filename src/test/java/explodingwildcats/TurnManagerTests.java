18ad0981ad6902814af8fc526fd2664ba296b97e
Brennan Benson <brennankbenson@gmail.com>
2024-12-09 20:51:07 -0600

    playCardLoop_UserInputShuffle_callsShuffle_doesNotEndTurn_secondUserInputTargetedAttack_callsDoTargetedAttack_endsTurn passes

--- src/test/java/explodingwildcats/TurnManagerTests.java
+++ src/test/java/explodingwildcats/TurnManagerTests.java
@@ -1315,6 +1315,54 @@ public class TurnManagerTests {
 
     EasyMock.verify(turnManager, gameEngine, ui);
   }
+
+  @Test
+  public void playCardLoop_UserInputShuffle_callsShuffle_doesNotEndTurn_secondUserInputTargetedAttack_callsDoTargetedAttack_endsTurn() {
+    GameEngine gameEngine = EasyMock.createMock(GameEngine.class);
+    UserInterface ui = EasyMock.createMock(UserInterface.class);
+    TurnManager turnManager = EasyMock.partialMockBuilder(TurnManager.class)
+            .withConstructor(ui, gameEngine)
+            .addMockedMethod("endTurn")
+            .addMockedMethod("getPlayableCard")
+            .addMockedMethod("doShuffle")
+            .addMockedMethod("doTargetedAttack")
+            .createMock();
+
+    int currPlayerIndex = 0;
+    turnManager.currPlayerIndex = currPlayerIndex;
+
+    String userInput = "shuffle";
+    Card userInputCard = Card.SHUFFLE;
+    boolean playerHasCard = true;
+    EasyMock.expect(ui.promptPlayCard(false)).andReturn(userInput);
+    EasyMock.expect(turnManager.getPlayableCard(userInput)).andReturn(userInputCard);
+    EasyMock.expect(gameEngine.playerHasCard(userInputCard, currPlayerIndex)).andReturn(playerHasCard);
+
+    turnManager.doShuffle();
+    EasyMock.expectLastCall().andAnswer(() -> {
+      turnManager.playerTurnHasEnded = false; // do not terminate loop
+      return null;
+    });
+
+    String newUserInput = "targeted attack";
+    Card newUserInputCard = Card.TARGETED_ATTACK;
+    boolean newPlayerHasCard = true;
+    EasyMock.expect(ui.promptPlayCard(false)).andReturn(newUserInput);
+    EasyMock.expect(turnManager.getPlayableCard(newUserInput)).andReturn(newUserInputCard);
+    EasyMock.expect(gameEngine.playerHasCard(newUserInputCard, currPlayerIndex)).andReturn(newPlayerHasCard);
+
+    turnManager.doTargetedAttack();
+    EasyMock.expectLastCall().andAnswer(() -> {
+      turnManager.playerTurnHasEnded = true; // Manually terminate loop
+      return null;
+    });
+
+    EasyMock.replay(turnManager, gameEngine, ui);
+
+    turnManager.playCardLoop();
+
+    EasyMock.verify(turnManager, gameEngine, ui);
+  }
 }
 
 
